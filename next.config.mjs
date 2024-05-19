69fea9507cf5ac4e453c12dcb89fbfb01c27df40
qthwng01 <qthwng.01@gmail.com>
2024-05-20 01:14:20 +0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+//   output: 'export',
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
   images: {
     domains: ['img.phimapi.com', 'img.ophim.live'],
   },
